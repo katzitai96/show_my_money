1d12da2ddc645830193ef705972c3365efbd1d42
Itai Katz <katzitai96@gmail.com>
2025-03-02 16:09:32 +0200

    changing and fixing scrap to work on github action

--- lib/scraper/getTransaction.ts
+++ lib/scraper/getTransaction.ts
@@ -20,15 +20,10 @@ export default async function getTransactions(year: number, month: number): Prom
   all_transactions = all_transactions.concat(new_transaction);
   new_transaction = await scraper(CompanyTypes.otsarHahayal, { username: "VZ0EFV1", password: "Taabura103!" }, year, month);
   all_transactions = all_transactions.concat(new_transaction);
-  // new_transaction = await scraper(CompanyTypes.visaCal, { username: "katzitai", password: "Tihnun103" }, year, month);
-  // all_transactions = all_transactions.concat(new_transaction);
+  new_transaction = await scraper(CompanyTypes.visaCal, { username: "katzitai", password: "Tihnun103" }, year, month);
+  all_transactions = all_transactions.concat(new_transaction);
 
   const supabase = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_KEY);
-
-  // await supabase
-  //   .from("transactions")
-  //   .insert({ amount: 100, description: "check", year: 2025, month: 1, day: 5, category: "מסעדות" })
-  //   .select();
 
   const { data, error } = await supabase.from("transactions").insert(
     all_transactions.map((transaction) => {

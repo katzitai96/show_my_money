b8f958eafa599e53ee915c8703b83e538d49441b
Itai Katz <katzitai96@gmail.com>
2025-03-08 16:21:15 +0200

    removing categories mapping on counting client side

--- app/Expenses/start_monthly_count.ts
+++ app/Expenses/start_monthly_count.ts
@@ -3,29 +3,6 @@ import { Transaction } from "../../lib/scraper/scraper";
 import { PRIVATE_SUPABASE_SERVICE_KEY, PUBLIC_SUPABASE_URL } from "../../lib/config";
 import { createClient } from "@supabase/supabase-js";
 
-export let category_mapping: Map<string, string> = new Map<string, string>([
-  ["מסעדות/קפה", "מסעדות/קפה"],
-  ["פארמה", "מכולת/סופר"],
-  ["מעדניות", "מכולת/סופר"],
-  ["שרות רפואי", "רפואה"],
-  ["רפואה", "רפואה"],
-  ["מכולת/סופר", "מכולת/סופר"],
-  ["דלק", "דלק"],
-  ["תקשורת", "תקשורת"],
-  ["תרבות", "פנאי ובילויים"],
-  ["פנאי ובילויים", "פנאי ובילויים"],
-  ["העברות כספים פייבוקס D/MS", "העברות כספים"],
-  ["העברות כספים", "העברות כספים"],
-  ["שירותי רכב", "הוצאות רכב"],
-  ["הוצאות רכב", "הוצאות רכב"],
-  ["בתי ספר", "לימודים"],
-  ["לימודים", "לימודים"],
-  ["שכר דירה", "שכר דירה"],
-  ["מילואים", "מילואים"],
-  ["משכורת", "משכורת"],
-  ["אחר", "אחר"],
-]);
-
 export let category_images: Map<string, string> = new Map<string, string>([
   ["מסעדות/קפה", "restaurant.svg"],
   ["מכולת/סופר", "shopping-cart.svg"],
@@ -60,21 +37,12 @@ export default async function start_monthly_count(year: number, month: number) {
     .then((data) => {
       return data.data as Array<Transaction>;
     });
-
-  //change the category to the category in the category_mapping
-  all_transactions.forEach((transaction: Transaction) => {
-    const category = category_mapping.get(transaction.category);
-    if (category) {
-      transaction.category = category;
-    } else {
-      transaction.category = transaction.category;
-    }
-  });
   // console.log(all_transactions);
+
   //create a map to count the amount of each category
   let category_count = new Map<string, number>();
   all_transactions.forEach((transaction: Transaction) => {
-    const category = category_mapping.get(transaction.category);
+    const category = transaction.category;
     if (category && category_count.has(category)) {
       let cur_amount = category_count.get(category);
       if (cur_amount != undefined) {
